6160423d861de72cc77e1c2f63f5cd8402f9d029
Srinivas <srinivas.net2516@gmail,com>
2022-07-07 23:39:34 +0530

    Practice : InsertAtHead and Display Linked List

---
+++ LisnkedListDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using LinkedListDemo;
+using System;
+using System.Collections.Generic;
+
+namespace LisnkedListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            list.InsertAtHead(100);
+            list.InsertAtHead(200);
+            list.InsertAtHead(100);
+            list.InsertAtHead(100);
+            list.InsertAtHead(300);
+            list.InsertAtHead(800);
+            var head = list.GetHead();
+            list.DiaplayLinkedList(head);
+
+
+        }
+    }
+}
