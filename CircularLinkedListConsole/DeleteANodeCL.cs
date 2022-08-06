719c75c78d963270f48f0dafadcaf3652fca7a0c
Srinivas <sannavarpu@affinitiv.com>
2022-08-06 16:47:48 +0530

    Circular Linked List Implementation

---
+++ CircularLinkedListConsole/DeleteANodeCL.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircularLinkedListConsole
+{
+    public class DeleteANodeCL
+    {
+        Node head;
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+            if(head == null)
+            {
+                newNode.next = newNode;
+                head = newNode;
+            }
+            else
+            {
+                Node last = head;
+                do
+                {
+                    last = last.next;
+                }while(last.next != head);
+                last.next = newNode;
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                newNode.next = newNode;
+                head = newNode;
+            }
+            else
+            {
+                newNode.next = head;
+                
+                Node last = head;
+                do
+                {
+                    last = last.next;
+                } while (last.next != head);
+                head = newNode;
+                last.next = head;
+            }
+        }
+
+        public void print()
+        {
+            Node last = head;
+            do
+            {
+                Console.WriteLine(last.data);
+                last = last.next;
+            } while (last != head);
+        }
+
+        public void Delete(int key)
+        {
+            var last = head;
+
+            do
+            {
+                if (last.next.data == key)
+                {
+                    last.next = last.next.next;
+                    break;
+                }
+                else
+                {
+                    last = last.next;
+                }
+            } while (last.next != head);
+        }
+    }
+}
---
+++ CircularLinkedListConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CircularLinkedListConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Node head = new Node(10);
+            //Node middle = new Node(20);
+            //Node last = new Node(30);
+
+            //head.next = middle;
+            //middle.next = last;
+            //last.next = head;
+
+            //InsertAtBeginningCL insertAtBeginningCL = new InsertAtBeginningCL();
+            //insertAtBeginningCL.AddFirst(90);
+            //insertAtBeginningCL.AddFirst(80);
+            //insertAtBeginningCL.AddFirst(60);
+            //insertAtBeginningCL.AddFirst(100);
+
+            //insertAtBeginningCL.Print();
+
+            //InsertAtEndCL insertAtEndCL = new InsertAtEndCL();
+
+            //insertAtEndCL.AddLast(10);
+            //insertAtEndCL.AddLast(230);
+
+            //insertAtEndCL.Print();
+
+            //SearchANode searchANode = new SearchANode();
+            //searchANode.AddLast(90);
+            //searchANode.AddLast(70);
+            //searchANode.AddLast(80);
+            //searchANode.AddFirst(10);
+            //searchANode.print();
+            //searchANode.Search(190);
+
+            DeleteANodeCL deleteANodeCL = new DeleteANodeCL();
+            deleteANodeCL.AddLast(180);
+            deleteANodeCL.AddLast(170);
+            deleteANodeCL.AddLast(160);
+            deleteANodeCL.AddLast(150);
+            deleteANodeCL.print();
+            Console.WriteLine();
+            deleteANodeCL.AddFirst(100);
+            deleteANodeCL.print();
+            Console.WriteLine();
+            deleteANodeCL.Delete(160);
+            deleteANodeCL.print();
+
+
+
+
+
+        }
+    }
+}
