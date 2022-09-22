47f65b1db0f06e82192d59edb7d88c510aed777e
Srinivas_Annavarpu <Srinivas_Annavarpu@W10F27WKG3>
2022-09-22 14:52:02 -0500

    Merge two sorted Linked List

---
+++ MergeTwoSortedLists21/LinkedList.cs
@@ -0,0 +1,45 @@
+﻿namespace MergeTwoSortedLists21
+{
+    public class LinkedList
+    {
+       
+
+        public ListNode CreateListFromArray(int[] arr)
+        {
+            ListNode head = null;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var newNode = new ListNode(arr[i]);
+                if (head == null)
+                {
+                    head = newNode;
+                }
+                else
+                {
+                    var curr = head;
+                    while (curr.next != null)
+                    {
+                        curr = curr.next;
+                    }
+                    curr.next = newNode;
+                }
+
+            }
+            return head;
+        }
+
+        public void DisplayLinkedList(ListNode head)
+        {
+            if (head == null) { System.Console.WriteLine("List is empty"); }
+
+            ListNode curr = head;
+            System.Console.Write("[");
+            while(curr!= null)
+            {
+                System.Console.Write(curr.val + (curr.next != null ? ", " : ""));
+                curr = curr.next;
+            }
+            System.Console.Write("]");
+        }
+    }
+}
---
+++ MergeTwoSortedLists21/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MergeTwoSortedLists21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            var list1 = linkedList.CreateListFromArray(new int[] { 1, 2, 4 });
+            var list2 = linkedList.CreateListFromArray(new int[] { 0, 1, 3, 4 });
+
+            linkedList.DisplayLinkedList(MergeTwoLists(list1, list2));
+        }
+
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null && list2 == null) return null;
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+
+            ListNode newList = new ListNode(-1);
+            ListNode node = newList;
+
+            while(list1 != null && list2 != null)
+            {
+                if(list1.val < list2.val)
+                {
+                    node.next = list1;
+                    list1 = list1.next;
+                    node = node.next;
+                }
+                else
+                {
+                    node.next = list2;
+                    list2 = list2.next;
+                    node = node.next;
+                }
+            }
+
+            while(list1!= null)
+            {
+                node.next = list1;
+                list1 = list1.next;
+                node = node.next;
+            }
+
+            while(list2 != null)
+            {
+                node.next = list2;
+                list2 = list2.next;
+                node = node.next;
+            }
+
+            return newList.next;
+        }
+    }
+}
